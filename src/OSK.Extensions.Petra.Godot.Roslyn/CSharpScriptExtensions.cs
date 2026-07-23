c08bfad1b0e4748adbccd77ab2f2e4973542cde7
BlankDev117 <mmmasent@gmail.com>
2026-07-22 22:24:45 -0400

    Modify CSharpScript Extension (OSK-19)
    Motivation
    ----
    The current extension for getting the class type for a CSharpScript in Godot is brittle. It relies on the res directory and only having a single class name and file with a given name,
    but this may not always be the case
    
    Modifications
    ----
    * Move the CSharpScript extension into a Roslyn package that utilizes rosyln to parse and interpret the source code file that a CSharpScript references
    
    Result
    ----
    The extension for getting a CSharpScript is more flexible, less brittle, and more powerful by allowing multiple identically named files

---
+++ src/OSK.Extensions.Petra.Godot.Roslyn/CSharpScriptExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Godot;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace OSK.Extensions.Petra.Godot.Roslyn
+{
+    public static class CSharpScriptExtensions
+    {
+        /// <summary>
+        /// Attempts to get the script's underlying type. This would return the script's actual class type. 
+        /// </summary>
+        /// <remarks>
+        /// 💡Notes:
+        /// <list type="bullet">
+        /// <item> This call assumes the script is within the <c>res://</c> directory </item>
+        /// </list>
+        /// </remarks>
+        /// <param name="script">The script to get the type for</param>
+        public static Type? GetScriptType(this CSharpScript script)
+        {
+            ArgumentNullException.ThrowIfNull(script);
+
+            if (string.IsNullOrWhiteSpace(script.SourceCode))
+            {
+                script.SourceCode = FileAccess.GetFileAsString(script.ResourcePath);
+                if (FileAccess.GetOpenError() != Error.Ok)
+                {
+                    return null;
+                }
+            }
+
+            var classDeclarationSyntax = CSharpSyntaxTree.ParseText(script.SourceCode).GetRoot()
+                                                   .DescendantNodes()
+                                                   .OfType<ClassDeclarationSyntax>()
+                                                   .FirstOrDefault();
+            if (classDeclarationSyntax is null)
+            {
+                GD.PushWarning($"Unable to parse the class reesource source code: {script.ResourcePath}.");
+                return null;
+            }
+
+            var namespaceNames = classDeclarationSyntax.Ancestors()
+                                                        .OfType<BaseNamespaceDeclarationSyntax>()
+                                                        .Reverse()
+                                                        .Select(syntax => syntax.Name.ToString());
+
+            var scriptTypeName = string.Join(".", namespaceNames.Append(classDeclarationSyntax.Identifier.Text));
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var scriptType = namespaceNames.Any()
+                    ? assembly.GetType(scriptTypeName)
+                    : assembly.GetTypes().FirstOrDefault(assemblyType => assemblyType.Name.Equals(scriptTypeName, StringComparison.Ordinal));
+                if (scriptType is not null)
+                {
+                    return scriptType;
+                }
+            }
+
+            return null;
+        }
+    }
+}
