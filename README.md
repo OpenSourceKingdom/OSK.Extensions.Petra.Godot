f60af032fc6fac65e57229fa4efd200c31538965
BlankDev117 <mmmasent@gmail.com>
2026-07-22 22:31:39 -0400

    readme adjustments and slight code enhancemnets

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ A project that provides many common and useful extensions for a variety Godot re
  - `[Node] FindNode<T>`: finds a node or nodes of a given type within the base node. This can be performed either for a parent or child.
  - `[Node] AddChildren`: adds many children nodes to a parent
  - `[Node] RemoveChildren | FreeChildren`: removes or frees all children from the parent
- - `[CSharpScript] GetScriptType`: gets the underlying script type, which is the actual C# class type
 
 # OSK.Extensions.Petra.Godot.DependencyInjection
 
 A project that provides commond and useful extensions relating to the dependency injection system. Extensions include:
  - `ServiceCollection`: Adds a node to a dependency container that utilizes standard .NET service collection and providers
+	- 
+
+# OSK.Extensions.Petra.Godot.Roslyn
+
+ - `[CSharpScript] GetScriptType`: gets the underlying script type, which is the actual C# class type. This method works for class declarations and will accomodate identically named files within a project.
--- src/OSK.Extensions.Petra.Godot.Roslyn/CSharpScriptExtensions.cs
+++ src/OSK.Extensions.Petra.Godot.Roslyn/CSharpScriptExtensions.cs
@@ -4,64 +4,70 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Xml.Linq;
 
-namespace OSK.Extensions.Petra.Godot.Roslyn
+namespace OSK.Extensions.Petra.Godot.Roslyn;
+
+public static class CSharpScriptExtensions
 {
-    public static class CSharpScriptExtensions
+    private static readonly Dictionary<string, Type?> _knwonScriptTypes = [];
+
+    /// <summary>
+    /// Attempts to get the script's underlying type. This would return the script's actual class type. 
+    /// </summary>
+    /// <remarks>
+    /// 💡Notes:
+    /// <list type="bullet">
+    /// <item> If the source code is not immediately available for the script, it will be loaded from the file located at the ResourcePath </item>
+    /// <item> This method currently only supports class style declarations </item>
+    /// </list>
+    /// </remarks>
+    /// <param name="script">The script to get the type for</param>
+    public static Type? GetScriptType(this CSharpScript script)
     {
-        /// <summary>
-        /// Attempts to get the script's underlying type. This would return the script's actual class type. 
-        /// </summary>
-        /// <remarks>
-        /// 💡Notes:
-        /// <list type="bullet">
-        /// <item> This call assumes the script is within the <c>res://</c> directory </item>
-        /// </list>
-        /// </remarks>
-        /// <param name="script">The script to get the type for</param>
-        public static Type? GetScriptType(this CSharpScript script)
+        ArgumentNullException.ThrowIfNull(script);
+
+        if (string.IsNullOrWhiteSpace(script.SourceCode))
         {
-            ArgumentNullException.ThrowIfNull(script);
-
-            if (string.IsNullOrWhiteSpace(script.SourceCode))
+            script.SourceCode = FileAccess.GetFileAsString(script.ResourcePath);
+            if (FileAccess.GetOpenError() != Error.Ok)
             {
-                script.SourceCode = FileAccess.GetFileAsString(script.ResourcePath);
-                if (FileAccess.GetOpenError() != Error.Ok)
-                {
-                    return null;
-                }
-            }
-
-            var classDeclarationSyntax = CSharpSyntaxTree.ParseText(script.SourceCode).GetRoot()
-                                                   .DescendantNodes()
-                                                   .OfType<ClassDeclarationSyntax>()
-                                                   .FirstOrDefault();
-            if (classDeclarationSyntax is null)
-            {
-                GD.PushWarning($"Unable to parse the class reesource source code: {script.ResourcePath}.");
                 return null;
             }
+        }
 
-            var namespaceNames = classDeclarationSyntax.Ancestors()
-                                                        .OfType<BaseNamespaceDeclarationSyntax>()
-                                                        .Reverse()
-                                                        .Select(syntax => syntax.Name.ToString());
-
-            var scriptTypeName = string.Join(".", namespaceNames.Append(classDeclarationSyntax.Identifier.Text));
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                var scriptType = namespaceNames.Any()
-                    ? assembly.GetType(scriptTypeName)
-                    : assembly.GetTypes().FirstOrDefault(assemblyType => assemblyType.Name.Equals(scriptTypeName, StringComparison.Ordinal));
-                if (scriptType is not null)
-                {
-                    return scriptType;
-                }
-            }
-
+        var classDeclarationSyntax = CSharpSyntaxTree.ParseText(script.SourceCode).GetRoot()
+                                               .DescendantNodes()
+                                               .OfType<ClassDeclarationSyntax>()
+                                               .FirstOrDefault();
+        if (classDeclarationSyntax is null)
+        {
+            GD.PushWarning($"Unable to parse the class reesource source code: {script.ResourcePath}.");
             return null;
         }
+
+        var namespaceNames = classDeclarationSyntax.Ancestors()
+                                                    .OfType<BaseNamespaceDeclarationSyntax>()
+                                                    .Reverse()
+                                                    .Select(syntax => syntax.Name.ToString());
+
+        var scriptTypeName = string.Join(".", namespaceNames.Append(classDeclarationSyntax.Identifier.Text));
+        if (_knwonScriptTypes.TryGetValue(scriptTypeName, out var scriptType))
+        {
+            return scriptType;
+        }
+
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            scriptType = namespaceNames.Any()
+                ? assembly.GetType(scriptTypeName)
+                : assembly.GetTypes().FirstOrDefault(assemblyType => assemblyType.Name.Equals(scriptTypeName, StringComparison.Ordinal));
+            if (scriptType is not null)
+            {
+                break;
+            }
+        }
+
+        _knwonScriptTypes[scriptTypeName] = scriptType;
+        return null;
     }
 }
